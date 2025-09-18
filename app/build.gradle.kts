031e11e314d368ac1a4a52da57892ab2a790c3a4
HuaLiMao-AQ <hualimao_aq@163.com>
2025-09-18 21:18:19 +0800

    libs: 添加Jetpack Compose所需要的依赖
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,10 +5,13 @@ plugins {
     id("com.google.dagger.hilt.android")
 
     // Kotlin
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.kotlin.android)
 
     // Kotlin Symbol Processing
     alias(libs.plugins.ksp)
+
+    // Jetpack Compose
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -34,6 +37,16 @@ android {
             )
         }
     }
+
+    // Jetpack Compose 支持
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -45,9 +58,21 @@ dependencies {
     // Android Core
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     // Hilt
     implementation(libs.hilt.android)
