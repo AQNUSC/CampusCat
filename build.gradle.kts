f8b8d17a2258ddf4837ab0d94c738f6e96ddd56d
HuaLiMao-AQ <hualimao_aq@163.com>
2025-09-18 21:18:19 +0800

    libs: 添加Jetpack Compose所需要的依赖
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,11 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
 
     // Kotlin
-    kotlin("android") version "2.2.0" apply false
+    alias(libs.plugins.kotlin.android) apply false
 
     // Kotlin Symbol Processing
     id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+
+    // Jetpack Compose
+    alias(libs.plugins.kotlin.compose) apply false
 }
