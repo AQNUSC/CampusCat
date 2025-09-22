1e6201253b918c1220f6e0f53fd04615817bdc57
HuaLiMao-AQ <hualimao_aq@163.com>
2025-09-22 20:31:17 +0800

    util: 添加LoginErrorCode枚举类以定义登录错误代码
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

---
+++ app/src/main/java/com/catboy/campuscat/util/enums/LoginErrorCode.java
@@ -0,0 +1,18 @@
+package com.catboy.campuscat.util.enums;
+
+/**
+ * LoginErrorCode
+ * <p>
+ *     枚举类，定义了登录过程中可能出现的错误代码。
+ * </p>
+ */
+public enum LoginErrorCode {
+    /** 学校统一登录返回错误码 */
+    PASSERROR,
+    NOUSER,
+    USERLOCK,
+    NOREGISTER,
+
+    /** 自定义错误码 */
+    UNKNOWN
+}
