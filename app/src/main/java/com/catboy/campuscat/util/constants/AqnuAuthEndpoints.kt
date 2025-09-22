3812229e5ac39a1ae0da1d6533f368b3fc420bb7
HuaLiMao-AQ <hualimao_aq@163.com>
2025-09-22 20:03:52 +0800

    util: 添加AqnuAuthEndpoints类及统一登录接口常量
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

---
+++ app/src/main/java/com/catboy/campuscat/util/constants/AqnuAuthEndpoints.kt
@@ -0,0 +1,14 @@
+package com.catboy.campuscat.util.constants
+
+/**
+ * AqnuAuthEndpoints
+ * 安庆师范大学统一登录接口
+ */
+object AqnuAuthEndpoints {
+    // 统一登录域名
+    const val DOMAIN: String = "ca.aqnu.edu.cn"
+    // 统一登录 URL
+    const val BASE_URL: String = "https://ca.aqnu.edu.cn/"
+    // 统一登陆Tickets获取端点
+    const val GET_TICKETS_ENDPOINT: String = "lyuapServer/v1/tickets"
+}
