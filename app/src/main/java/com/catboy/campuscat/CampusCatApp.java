74149f42256fe382c29160d2bfbbb9b028d7d715
HuaLiMao-AQ <hualimao_aq@163.com>
2025-09-22 16:18:23 +0800

    feat: TaskManager: 实现单例模式，添加初始化方法，优化任务执行逻辑
    不使用Hilt实现单例管理
    
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- app/src/main/java/com/catboy/campuscat/CampusCatApp.java
+++ app/src/main/java/com/catboy/campuscat/CampusCatApp.java
@@ -2,18 +2,12 @@ package com.catboy.campuscat;
 
 import android.app.Application;
 
-import com.catboy.campuscat.taskqueue.TaskManager;
-
-import javax.inject.Inject;
-
-import dagger.hilt.android.HiltAndroidApp;
 import timber.log.Timber;
 
 /**
  * CampusCatApp类是应用程序的入口点，继承自Application类。
  * 使用@HiltAndroidApp注解启用Hilt依赖注入框架。
  */
-@HiltAndroidApp
 public class CampusCatApp extends Application {
 
     @Override
