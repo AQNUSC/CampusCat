fdeb9f4ea6ccd297a7e1cd24a86f6938bd7c777f
HuaLiMao-AQ <hualimao_aq@163.com>
2025-09-22 20:26:04 +0800

    FIX: data: response: 添加Lombok注解以简化ResponseBodyData和ResponseBodyMeta类
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- app/src/main/java/com/catboy/campuscat/data/model/response/ResponseBodyData.java
+++ app/src/main/java/com/catboy/campuscat/data/model/response/ResponseBodyData.java
@@ -1,11 +1,14 @@
 package com.catboy.campuscat.data.model.response;
 
+import lombok.Data;
+
 /**
  * Data
  * <p>
  *     用于封装响应数据的类，包含响应代码和具体数据内容。
  * </p>
  */
+@Data
 public class ResponseBodyData {
     private String code;
     private String data;
--- app/src/main/java/com/catboy/campuscat/data/model/response/ResponseBodyMeta.java
+++ app/src/main/java/com/catboy/campuscat/data/model/response/ResponseBodyMeta.java
@@ -1,11 +1,14 @@
 package com.catboy.campuscat.data.model.response;
 
+import lombok.Data;
+
 /**
  * Meta
  * <p>
  *     用于封装响应的元信息，包括请求是否成功、状态码和消息。
  * </p>
  */
+@Data
 public class ResponseBodyMeta {
     private boolean success;
     private int statusCode;
