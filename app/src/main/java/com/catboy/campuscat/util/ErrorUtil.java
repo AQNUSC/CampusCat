a0af3be70c7afa514b7697bb53942fbd851cda82
HuaLiMao-AQ <hualimao_aq@163.com>
2025-09-22 22:21:47 +0800

    util: ErrorUtil: 修改getErrorMessage方法以使用TaskResult替代LoginResponse
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- app/src/main/java/com/catboy/campuscat/util/ErrorUtil.java
+++ app/src/main/java/com/catboy/campuscat/util/ErrorUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.catboy.campuscat.R;
 import com.catboy.campuscat.data.model.response.LoginResponse;
+import com.catboy.campuscat.taskqueue.TaskResult;
 import com.catboy.campuscat.util.enums.LoginErrorCode;
 
 /**
@@ -18,41 +19,36 @@ public class ErrorUtil {
      * 获取登录错误信息
      *
      * @param context  上下文对象，用于获取资源
-     * @param response 登录响应对象，包含错误代码和数据
+     * @param taskResult 包含登录结果的任务结果对象
      * @return 对应的错误信息字符串
      */
-    public static String getErrorMessage(Context context, LoginResponse response) {
+    public static String getErrorMessage(Context context, TaskResult<LoginErrorCode> taskResult) {
         // 获取程序上下文
         Context appContext = context.getApplicationContext();
 
         // 获取错误代码
-        LoginErrorCode errorCode = null;
-        try {
-            errorCode = LoginErrorCode.valueOf(response.getData().getCode());
-        } catch (Exception e) {
-            errorCode = LoginErrorCode.UNKNOWN;
-        }
+        LoginErrorCode errorCode = taskResult.getData();
 
         // 根据错误代码返回对应的错误信息
         switch (errorCode) {
             case NOUSER:
                 return appContext.getString(R.string.login_error_no_user);
             case PASSERROR:
-                String errorMaxTimes = response.getData().getData().split(",")[0];
-                String errorNumber = response.getData().getData().split(",")[1];
+                String errorMaxTimes = taskResult.getMessage().split(",")[0];
+                String errorNumber = taskResult.getMessage().split(",")[1];
                 if (errorNumber.equals(errorMaxTimes)) {
                     return appContext.getString(R.string.login_error_password_incorrect_max);
                 } else {
                     return appContext.getString(R.string.login_error_password_incorrect, errorNumber, errorMaxTimes);
                 }
             case USERLOCK:
-                String errorMessage = response.getData().getData();
+                String errorMessage = taskResult.getMessage();
                 String unlockTime = errorMessage.substring(11);
                 return appContext.getString(R.string.login_error_user_locked, unlockTime);
             case NOREGISTER:
                 return appContext.getString(R.string.login_error_no_register);
             default:
-                return appContext.getString(R.string.login_error_unknown);
+                return String.format(appContext.getString(R.string.login_error_unknown), taskResult.getError());
         }
     }
 }
